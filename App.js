bb3a923aba0936e4e8c63655ecbc2531d34cee39
dev-abhinavsinghHP <abhinavkumarsinghHP@gmail.com>
2023-04-24 13:13:47 +0530

    second commit

--- App.js
+++ App.js
@@ -1,22 +1,21 @@
 import React from "react";
 
 import { ScrollView } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import List from "./Components/List";
 import Sectionlist from "./Components/Sectionlist";
 import Heading from "./Components/Heading";
 import TodoForm from "./Components/TodoForm";
 
-const Stack = createNativeStackNavigator();
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Heading" component={Heading} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ScrollView>
+      <Heading />
+      <List />
+      <Sectionlist />
+
+      <TodoForm />
+    </ScrollView>
   );
 }
 
